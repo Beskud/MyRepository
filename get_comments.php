ed349697ea5ef9dd445ac5e670e749c4770727b3
Beskud <131443148+Beskud@users.noreply.github.com>
2023-04-21 12:11:52 +0300

    Delete get_comments.php

--- get_comments.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$dbh = new PDO('mysql:host=localhost;dbname=page', 'root', '');
-$result = $dbh->query('SELECT * FROM Comment');
-$row = $result->fetchAll(PDO::FETCH_ASSOC);
-echo json_encode($row);
